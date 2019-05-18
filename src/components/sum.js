be8b9228a9444547fc3160a50e58a3a85e401871
walterlee <walter.lee9@gmail.com>
2019-05-18 15:57:43 -0500

    added other math operations to sum.js

--- src/components/sum.js
+++ src/components/sum.js
@@ -1,4 +1,17 @@
 function sum(a, b) {
     return a + b;
 }
-module.exports = sum;
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+module.exports = { sum, subtract, multiply, divide };
--- src/tests/sum.test.js
+++ src/tests/sum.test.js
@@ -1,5 +1,17 @@
-const sum = require('../components/sum');
+const { sum, subtract, multiply, divide } = require('../components/sum');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('subtracts 2 - 1 to equal 1', () => {
+    expect(subtract(2, 1)).toBe(1);
+});
+
+test('multiply 2 * 2 to equal 4', () => {
+    expect(multiply(2, 2)).toBe(4);
+});
+
+test('divide 4 / 2 to equal 2', () => {
+    expect(divide(4, 2)).toBe(2);
 });
