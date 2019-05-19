b1594d4d11792a981108f21d47ad07ef827b830e
walterlee <walter.lee9@gmail.com>
2019-05-19 16:08:45 -0500

    fizzbuzz test

---
+++ src/components/fizzbuzz.js
@@ -0,0 +1,33 @@
+function monkeybars(a) {
+    if (a % 15 === 0) {
+        return "monkeybars";
+    }
+    if (a % 3 === 0) {
+        return "monkey";
+    }
+    if (a % 5 === 0) {
+        return "bars";
+    }
+}
+
+function fizzbuzz(a) {
+    for (let i = 1; i <= a; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        }
+        else if (i % 3 === 0) {
+            console.log('fizz');
+        }
+        else if (i % 5 === 0) {
+            console.log('buzz');
+        }
+        // else {
+        //     console.log(i);
+        // }
+    }
+}
+
+// monkeybars(3);
+// fizzbuzz(3);
+
+module.exports = { fizzbuzz, monkeybars };
---
+++ src/tests/fizzbuzz.test.js
@@ -0,0 +1,21 @@
+const { fizzbuzz, monkeybars } = require('../components/fizzbuzz');
+
+test('takes 3 and outputs monkey', () => {
+    expect(monkeybars(3)).toBe('monkey');
+});
+
+test('takes 5 and outputs bars', () => {
+    expect(monkeybars(5)).toBe('bars');
+});
+
+test('takes 15 and outputs monkeybars', () => {
+    expect(monkeybars(15)).toBe('monkeybars');
+});
+
+test('takes 3 and outputs fizz', () => {
+    expect(fizzbuzz(3)).toBe('fizz');
+});
+
+// test('takes 5 and outputs buzz', () => {
+//     expect(fizzbuzz(5)).toBe('buzz');
+// });
